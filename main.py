4b62e45790cd4fe4844f18549124c1e1f2c86d06
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2023-07-04 00:56:36 -0300

    primeiro teste na criação do dictionary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-
+#Projeto de Python: entendendo a Orientação a Objetos
+#Leonardo Medeiros Silva Aparicio
 
 def cria_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
+
