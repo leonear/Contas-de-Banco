0a9106ac04e4928001edd1ab307578e86f84a74d
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2023-07-04 01:12:46 -0300

    Criação de funções deposito, saque e extrato

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def cria_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
 
+def deposito(conta, valor):
+    conta["saldo"] += valor
+
+def saque(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
