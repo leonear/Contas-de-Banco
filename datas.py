bcfd94089cfad56303a111cb4176b85e92f4079c
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2023-07-06 03:05:43 -0300

    desafio opcional: python entendendo a orientação a objetos

---
+++ datas.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+class Data:
+
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def formatada(self):
+        print("{}/{}/{}".format(self.dia, self.mes, self.ano))
